d5caf09d4525e31eba005e959aed04e68ce538c5
Petru Marginean <petru.marginean@gmail.com>
2018-12-09 07:57:27 -0500

    misc

--- main.cpp
+++ main.cpp
@@ -2,22 +2,30 @@
 #include <vector>
 
 template <typename T>
+void print(std::vector<T>& path) {
+    for (auto e: path) {
+        std::cout << "'" << e << "', ";
+    }
+    std::cout << "\n";
+}
+
+template <typename T>
 void genPerms(std::vector<T>& path, size_t permLength) {
     if (path.size() == permLength) {
+        print(path);
         return;
     }
-    //if (!promising(path, permLength)) {
-    //    return;
-    //}
     for (size_t i = permLength; i < path.size(); ++i) {
-
+        std::swap(path[permLength], path[i]);
+        genPerms(path, permLength + 1);
+        std::swap(path[permLength], path[i]);
     }
 }
 
 int main() {
     std::string s = "abc";
     std::vector<char> v(s.begin(), s.end());
-    genPerms(v, 2);   
+    genPerms(v, 0);   
     return 0;
 }
 
